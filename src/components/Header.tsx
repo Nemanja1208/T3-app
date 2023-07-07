e766057a5d6d56e068c001c67bd6b32e3129330f
Nemanja1208 <nevaljao994@gmail.com>
2023-07-07 23:47:50 +0200

    smallChanges

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import Image from "next/image";
 
 export const Header = () => {
   const { data: sessionData } = useSession();
@@ -21,8 +20,6 @@ export const Header = () => {
                 <img
                   src={sessionData?.user?.image ?? ""}
                   alt={sessionData?.user?.name ?? ""}
-                  //   width={100}
-                  //   height={100}
                 />
               </div>
             </label>
